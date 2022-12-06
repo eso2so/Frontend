116fed2fa4fe99b5c354557aa4351ffa0f776451
eso2so <117644740+eso2so@users.noreply.github.com>
2022-12-06 21:21:23 +0900

    20221206 시험 이소영

---
+++ JavaScript/test.js
@@ -0,0 +1,19 @@
+// userList 안의 객체들 중 age 프로퍼티의 값이 30 이상인 객체의 이름만 모아서 result 배열에 담기
+
+// 배열의 고차함수들을 이용 => 필터링과 데이터 변형
+let userList = [
+    { name: "Minsu", age: 20 },
+    { name: "Seok", age: 30 },
+    { name: "Yeji", age: 23 },
+    { name: "Minseok", age: 43 },
+];
+
+let result = userList.filter(function (age) {
+    age >= 30;
+});
+console.log(result, numList);
+
+let result02 = userList.map(function (age) {
+    age >= 30;
+});
+console.log(result02, numList);
